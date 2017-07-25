2ac4b34cdd427cf76b8598a16e9751737466711f
James Lawson <jameslawson@users.noreply.github.com>
2017-07-25 09:36:14 +0100

    Add output code

--- scripts/tdd_status_output.sh
+++ scripts/tdd_status_output.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+source "$CURRENT_DIR/shared.sh"
 
-source $CURRENT_DIR/shared.sh
+
+TDD_STATUS_DIRS=$(get_tmux_option "@tdd_status_dirs" "")
+PANE_DIR="$(tmux display-message -p -F "#{pane_current_path}" -t0)"
+
+check_directory() {
+  # check if $PANE_DIR is inside $TDD_STATUS_DIRS
+  # which is comma-separated list of dirs
+  # https://stackoverflow.com/a/29301172/3649209
+  if [[ ",$TDD_STATUS_DIRS," = *",$PANE_DIR,"* ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 print_status() {
-  printf "Hello"
+  color_fail="#[bg=red] #[fg=black]"
+  color_pass="#[bg=green] #[fg=black]"
+  color_none="#[bg=colour8] #[fg=gray]"
+
+  if check_directory; then
+    exit_code=$(cd "$PANE_DIR" && npm run unit > /dev/null 2>&1 && echo $?)
+    if [ $exit_code -eq 0 ]; then
+      printf "Unit Tests: ${color_pass}PASSING"
+    else
+      printf "Unit Tests: ${color_fail}FAILING"
+    fi
+  else
+    printf "Unit Tests: ${color_none}  N/A  "
+  fi
 }
 
 main() {
--- tdd_status.tmux
+++ tdd_status.tmux
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 tdd_status_output="#($CURRENT_DIR/scripts/tdd_status_output.sh)"
 tdd_status_interpolation_string="\#{tdd_status}"
 
@@ -17,6 +16,7 @@ update_tmux_option() {
   local option="$1"
   local option_value="$(get_tmux_option "$option")"
   local new_option_value="$(do_interpolation "$option_value")"
+  echo "$new_option_value"
   set_tmux_option "$option" "$new_option_value"
 }
 
